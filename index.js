b3c0326bf1bbf4664668035b8c30acf24c677432
riannalei <riannalei@gmail.com>
2024-08-24 00:11:50 -0700

    implementing database

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+mongoose.connect(keys.mongoURI)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log('MongoDB connection error:', err));
 
 const app = express();
 
@@ -21,5 +23,5 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
