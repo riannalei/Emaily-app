ca3932b1acd48e1842a13e615b5f43f6a7e34c50
riannalei <riannalei@gmail.com>
2024-08-28 23:00:36 -0700

    client-side survey creation

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,17 @@
-import "materialize-css/dist/css/materialize.min.css";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { thunk } from "redux-thunk";
+import 'materialize-css/dist/css/materialize.min.css';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { thunk } from 'redux-thunk'; 
 
-import App from "./components/App";
-import reducers from "./reducers";
-import axios from "axios";
+
+import App from './components/App';
+import reducers from './reducers';
+import axios from 'axios';
 window.axios = axios;
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
-const el = document.getElementById("root");
+const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 root.render(
